66e840a241101a6c66f18175064bdf279bcd66e1
Calixto <deividsoncs@gmail.com>
2022-08-07 16:39:18 -0300

    Teminando mais exemplos de hello world e diferenças typescrip x ecma6 x vanilla(ecma5)

---
+++ hello-typescript/main.ts
@@ -0,0 +1,27 @@
+
+//tsc main.ts to compile examples
+
+//JS vanilla (ECMA5 - 2009)
+var minhaVar = 'minha variavel';
+
+//JS vanilla (ECMA5 - 2009)
+function minhaFuncao(x, y) {
+    return x + y;
+}
+
+//ECMA6
+let num = 2;
+
+//ECMA6
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+//JS vanilla (ECMA5 - 2009)
+numeros.map(function(n) {
+    return n*2
+})
+
+//JS ECMA6 - 2015
+numeros.map(n => n*2)
+
--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'meu-primeiro-component',
-    template: `
-        <p>Meu Primeiro Component com Angular2!</p>
-    `
+  selector: 'meu-primeiro-component',
+  template: `
+        <h3>(meu-primeiro)-Component com Angular2!</h3>
+    `,
 })
-export class MeuPrimeiroComponent { }
+export class MeuPrimeiroComponent {}
