61f78aa4ad6948100d877ef4413bacd94000d7a6
Calixto <deividsoncs@gmail.com>
2022-08-10 20:20:53 -0300

    aula 11 - event binds

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,17 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  styles: [`
+    .highlight{
+      background-color: yellow;
+      font-weight: bold;
+    }
+  `]
 })
 export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.com';
 
-  url: string = "http://loiane.com";
-
-  urlImagem: string = "https://img.freepik.com/fotos-gratis/trabalhando-no-codigo_1098-19858.jpg";
+  urlImagem: string =
+    'https://img.freepik.com/fotos-gratis/trabalhando-no-codigo_1098-19858.jpg';
 
   cursoAngular: boolean = true;
 
-  getValor(){
+  valorAtual: string = "";
+
+  valorSalvo: string = "";
+
+  isMouseOver: boolean = false;
+
+  getValor() {
     return 1;
   }
 
@@ -21,9 +33,26 @@ export class DataBindingComponent implements OnInit {
     return true;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  botaoClicado(){
+    return alert('Botão clicado!');
   }
 
+  onKeyUp(evento: KeyboardEvent){
+    let auxInput = (<HTMLInputElement> (evento.target) ).value
+
+    console.log( auxInput );
+    this.valorAtual = auxInput;
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  setMouse(mouseState: boolean){
+    this.isMouseOver = mouseState;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
