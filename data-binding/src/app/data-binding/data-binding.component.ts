80dbaf4cdb7ef9c33ac3287c1758b2c427b68bbb
Calixto <deividsoncs@gmail.com>
2022-08-10 18:44:19 -0300

    aular 09

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  url: string = "http://loiane.com"
+  url: string = "http://loiane.com";
+
+  urlImagem: string = "https://img.freepik.com/fotos-gratis/trabalhando-no-codigo_1098-19858.jpg";
+
+  cursoAngular: boolean = true;
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtir() {
+    return true;
+  }
 
   constructor() { }
 
