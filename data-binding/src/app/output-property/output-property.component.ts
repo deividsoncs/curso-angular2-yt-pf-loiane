769eb8455f30083191f72dc9ba4e884167bc8ec2
Calixto <deividsoncs@gmail.com>
2022-08-14 17:09:19 -0300

    aula 22 Angular-CLI

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -13,17 +13,24 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter(); 
 
+  //faz o bind resgatando o HTMLElement em tela para a lógica do componente
+  @ViewChild('campoInput') campoValorInput: ElementRef = Object();
+
   onClick(){
     alert("Clicou no botão!")
   }
 
   setPlus() {
-    this.valor++;
-    this.mudouValor.emit({novoValor: this.valor})
+    //this.valor++;
+    //this.mudouValor.emit({novoValor: this.valor});
+    
+    //poderia manipular o input aqui
+    console.log(this.campoValorInput)
+    this.campoValorInput.nativeElement.value++;
   }
 
   setMinus() {
-    this.valor--;
+   // this.valor--;
     this.mudouValor.emit({novoValor: this.valor})
   }
 
